00fe1c3b0a5fed434125e5623a5b16477ea93710
Ilyas SAQRANI <ilysaq@gmai.com>
2016-10-18 23:28:01 +0200

    Refactor BankTest

--- src/test/java/bank/BankTest.java
+++ src/test/java/bank/BankTest.java
@@ -2,25 +2,41 @@ package bank;
 
 import bank.account.Account;
 import bank.account.Client;
+import bank.transaction.Transaction;
+import bank.transaction.TransactionType;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BankTest {
 
     private static final int INITIAL_AMOUNT = 100;
     private Bank bank;
+    private List<Transaction> transactions;
     @Mock
     private Client client;
+    @Mock
+    private Account account;
 
     @Before
     public void initialise() {
         bank = new Bank();
+
+        transactions = new ArrayList<>();
+        transactions.add(new Transaction(TransactionType.DEPOSIT, INITIAL_AMOUNT));
+        transactions.add(new Transaction(TransactionType.WITHDRAWAL, 50));
+        transactions.add(new Transaction(TransactionType.WITHDRAWAL, 30));
+
+        given(account.getTransactions()).willReturn(transactions);
     }
 
     @Test
@@ -34,16 +50,12 @@ public class BankTest {
     public void
     should_create_a_new_account_with_initial_balance() {
         Account account = bank.createAccount(client, INITIAL_AMOUNT);
-        assertEquals(1, bank.getAccounts().size());
         assertEquals(INITIAL_AMOUNT, bank.calculateBalance(account));
     }
 
     @Test
     public void
     should_calculate_balance_after_withdrawals() {
-        Account account = bank.createAccount(client, INITIAL_AMOUNT);
-        bank.withdraw(account, 50);
-        bank.withdraw(account, 30);
         assertEquals(20, bank.calculateBalance(account));
     }
 
--- src/test/java/feature/WithdrawalFeature.java
+++ src/test/java/feature/WithdrawalFeature.java
@@ -1,8 +1,7 @@
 package feature;
 
-import org.junit.runner.RunWith;
-
 import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 public class WithdrawalFeature {
